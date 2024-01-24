c9531a05dfe9a437e22dc34dc706849543b02eb9
Abdul Mannan <abdulmann1234@gmail.com>
2024-01-25 00:11:06 +0500

    environment variable

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const apiKey = process.env.REACT_APP_API_KEY;
-const authDomain = process.env.REACT_APP__AUTH_DOMAIN;
+const authDomain = process.env.REACT_APP_AUTH_DOMAIN;
 const projectId = process.env.REACT_APP_PROJECTID;
 const storageBucket = process.env.REACT_APP_STORAGE_BUCKET;
 const messagingSenderId = process.env.REACT_APP_MESSAGING_SENDER_ID;
